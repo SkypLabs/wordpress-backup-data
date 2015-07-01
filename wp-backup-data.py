707e0d7c3ff329062aac3411f56f7006c167c762
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-07-02 00:00:01 +0200

    handle user interrupt (ctrl+c)

--- wp-backup-data.py
+++ wp-backup-data.py
@@ -61,43 +61,47 @@ ap.set_defaults(https=True)
 ap.set_defaults(prompt_pwd=False)
 args = vars(ap.parse_args())
 
-if not args["address"]:
-	address = check_prompt_field(check_address, 'URL: ')
-else:
-	address = check_field(check_address, args["address"])
+try:
+	if not args["address"]:
+		address = check_prompt_field(check_address, 'URL: ')
+	else:
+		address = check_field(check_address, args["address"])
 
-if not args["user"]:
-	user = check_prompt_field(check_username, 'Username: ')
-else:
-	user = check_field(check_username, args["user"])
+	if not args["user"]:
+		user = check_prompt_field(check_username, 'Username: ')
+	else:
+		user = check_field(check_username, args["user"])
 
-if args["prompt_pwd"] or not args["password"]:
-	password = check_prompt_field(check_password, password = True)
-else:
-	password = check_field(check_password, args["password"])
+	if args["prompt_pwd"] or not args["password"]:
+		password = check_prompt_field(check_password, password = True)
+	else:
+		password = check_field(check_password, args["password"])
 
-if args["https"]:
-	protocol = "https://"
-else:
-	protocol = "http://"
+	if args["https"]:
+		protocol = "https://"
+	else:
+		protocol = "http://"
 
-br = Browser()
-br.set_handle_robots(False)
+	br = Browser()
+	br.set_handle_robots(False)
 
-dt_format = "%Y-%m-%d_%H-%M-%S"
-dt = datetime.today()
-s = dt.strftime(dt_format)
-filename = address + "_" + s + ".xml"
+	dt_format = "%Y-%m-%d_%H-%M-%S"
+	dt = datetime.today()
+	s = dt.strftime(dt_format)
+	filename = address + "_" + s + ".xml"
 
-br.open(protocol + address + "/wp-login.php")
-br.select_form(name="loginform")
-br.form["log"] = user
-br.form["pwd"] = password
-br.submit()
+	br.open(protocol + address + "/wp-login.php")
+	br.select_form(name="loginform")
+	br.form["log"] = user
+	br.form["pwd"] = password
+	br.submit()
 
-br.open(protocol + address + "/wp-admin/export.php")
-br.select_form(nr = 0)
-backup = br.submit()
+	br.open(protocol + address + "/wp-admin/export.php")
+	br.select_form(nr = 0)
+	backup = br.submit()
 
-with open(filename, "w") as f:
-	f.write(backup.read())
+	with open(filename, "w") as f:
+		f.write(backup.read())
+except KeyboardInterrupt:
+	print("\n\nGood Bye\n")
+	exit(0)
